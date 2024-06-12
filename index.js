08a25d96f6682a5d3a31e92f185879b29c3dabea
advlabscoder <connectadvlabs@gmail.com>
2024-06-12 22:05:24 +0530

    fullstack by mak 5

--- index.js
+++ index.js
@@ -1,12 +1,54 @@
-// Use port number from the PORT environment variable or 3000 if not specified
-const port = process.env.PORT || 3000;
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+
+
+app.get('/api/read/all',async (req,res)=>{
+  try{
+   
+    res.send('hello ')
+  }catch(error){
+    res.send(error)
+  }
+
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+
+
+// Handle production
+if (process.env.NODE_ENV === 'production' || true) {
+  // Static folder
+  app.use(express.static(__dirname + '/public/'));
+
+  // Handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
+// const port = 3000
+
+// app.get('/', (req, res) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+// //  next();
+//   res.send('Hello World!')
+// })
+// app.use(cors)
+
+// app.listen(port, () => {
+//    console.log(`Example app listening on port ${port}`)
+//  })
+
+//  exports.api = functions.https.onRequest(app)
