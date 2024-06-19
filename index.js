fc740ae9bedfdd78c01be514e455fdc32d20250f
advlabscoder <connectadvlabs@gmail.com>
2024-06-19 22:10:45 +0530

    database interactive version

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express()
-
+const pgp = require('pg-promise')()
+const connectionstr = process.env.DATABASE_URL
+const db = pgp(connectionstr)
+// db.none('INSERT INTO userprofile(uid,username,password) VALUES ($1,$2,$3)',[1,"test","pass"]).then(()=>{
+//   console.log('success')
+// }).catch(error=>{console.log(error);})
+// console.log(db)
 
 
 app.use(express.json());
@@ -12,8 +18,9 @@ app.use(express.urlencoded({extended:true}))
 
 app.get('/api/read/all',async (req,res)=>{
   try{
-   
-    res.send('hello ')
+    const deviceData = await db.any('SELECT * FROM mcdata');
+    console.log(deviceData);
+    res.send(deviceData)
   }catch(error){
     res.send(error)
   }
@@ -21,6 +28,19 @@ app.get('/api/read/all',async (req,res)=>{
 })
 
 
+app.post('/api/devicedata', (req, res) => {
+  const reqData = req.body;
+ console.log( new Date().getTime())
+db.none('INSERT INTO mcdata(dev1data,dev2data) VALUES ($1,$2)',[req.body.dev1data,req.body.dev2data]).then(()=>{
+  console.log('success')
+  console.log('device data inserted')
+  res.send({
+    message: 'device data inserted'
+  });
+}).catch(error=>{console.log(error);})
+console.log(db)
+
+});
 
 
 // Handle production
--- public/assets/AboutView-B9HXjfcz.js
+++
@@ -1,1 +0,0 @@
-import{c as e,a,t as o,u as s,b as c,o as n}from"./index-CO1mAlYS.js";const r={class:"about"},_=c("h1",null,"Connecting Factory to Data Logic...",-1),p={__name:"AboutView",setup(i){const t=sessionStorage.getItem("data");return(u,l)=>(n(),e("div",r,[_,a(" testData "+o(s(t)),1)]))}};export{p as default};
